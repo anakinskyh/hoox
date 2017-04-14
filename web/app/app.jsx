8106d24f990b0d1a62cb78372efdb3f8a3531506
ElaneirzGard <elaneirzgard@gmail.com>
2017-04-14 22:01:26 +0700

    divide Recommend & Top10 in main page

--- web/app/app.jsx
+++ web/app/app.jsx
@@ -2,7 +2,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-ReactDOM.render(
-    <h1>app.jsx (main)</h1>,
-    document.getElementById('main-root')
-);
+//ReactDOM.render(
+//    <h1>app.jsx (main)</h1>,
+//    document.getElementById('main-root')
+//);
+
