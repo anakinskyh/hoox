5d3dbdbbd2cafe8621a506d5e083ce134994cae2
ElaneirzGard <elaneirzgard@gmail.com>
2017-04-14 22:08:43 +0700

    divide recommend-content  & top ten-content

--- web/app/app.jsx
+++ web/app/app.jsx
@@ -2,8 +2,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-//ReactDOM.render(
-//    <h1>app.jsx (main)</h1>,
-//    document.getElementById('main-root')
-//);
+ReactDOM.render(
+    <h1>Recommend</h1>,
+    document.getElementById('recommend-content')
+);
+
+
+ReactDOM.render(
+    <h1>Top10</h1>,
+    document.getElementById('topten-content')
+);
 
